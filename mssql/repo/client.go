6da189815af05fdb6156e3e563072ae0271d233f
Pavel Bastov <pbastov@gmail.com>
2014-11-03 17:56:13 +0600

    Return all types of clients from /api/clients

--- mssql/repo/client.go
+++ mssql/repo/client.go
@@ -18,10 +18,19 @@ func (repo MsSqlClientRepo) FindAll(db *sql.DB, offset int, limit int) ([]*model
 
 	var query =
 		"select id, name, client_type " +
-		"from (" +
-		"	select id, first_name + ' ' + last_name name, 'PERSON' client_type, " +
-		"		row_number() over (order by id asc) num " +
-		"	from dbo.Persons " +
+		"from ( " +
+		"	select *, " +
+		"		row_number() over (order by t.id asc) num " +
+		"	from ( " +
+		"		select id, first_name + ' ' + last_name name, 'PERSON' client_type " +
+		"		from dbo.Persons " +
+		"		union all " +
+		"		select id, name, 'COMPANY' from dbo.Corporates " +
+		"		union all " +
+		"		select id, name, 'GROUP' from dbo.Groups " +
+		"		union all " +
+		"		select id, name, 'VILLAGE_BANK' from dbo.Villages " +
+		"	) t " +
 		") t " +
 		"where t.num between ? and ?"
 	rows, err := db.Query(query, offset + 1, offset + limit)
