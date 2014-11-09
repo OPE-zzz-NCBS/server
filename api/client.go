6ecd3b80e74b97039a07cbef6d67a663e594808f
Pavel Bastov <pbastov@gmail.com>
2014-11-09 13:52:30 +0600

    Add delay for debugging

--- api/client.go
+++ api/client.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"log"
+	"time"
 	"github.com/OPENCBS/server/iface"
 	"github.com/OPENCBS/server/model"
 	"github.com/OPENCBS/server/util"
@@ -40,6 +41,8 @@ func GetClients(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		goto Error
 	}
+
+	time.Sleep(1000 * time.Millisecond)
 
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
 	w.Write(js)
