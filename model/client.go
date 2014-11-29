ed6d4673ada60ae91aa3295518639864f0ea7a16
Pavel Bastov <pbastov@gmail.com>
2014-11-30 02:07:17 +0600

    Simplify client model

--- model/client.go
+++ model/client.go
@@ -1,18 +1,9 @@
 package model
 
 type Client struct {
-	Href string `json:"href"`
 	Id int `json:"id"`
 	Name string `json:"name"`
 	Type string `json:"type"`
-}
-
-type Clients struct {
-	Href string `json:"href"`
-	Offset int `json:"offset"`
-	Limit int `json:"limit"`
-	Count int `json:"count"`
-	Items []*Client `json:"items"`
 }
 
 func NewClient() *Client {
